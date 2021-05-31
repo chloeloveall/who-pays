b84b9c05e46dab1266b8bb2c775303ab20b77dea
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-31 14:43:48 -0700

    Add basic project setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
-
+import { MyProvider } from './src/context/';
 import App from './App';
+
+const provider = () => (
+  <MyProvider>
+    <App />
+  </MyProvider>
+)
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(provider);
