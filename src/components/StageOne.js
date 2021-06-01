ce99419bfae0544b66769d53200e42bdd81e4119
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-31 20:17:03 -0700

    Add context to stage one, add formik with yup validation

--- src/components/StageOne.js
+++ src/components/StageOne.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { Input, Button, Text } from 'react-native-elements';
+import { MyContext } from '../context';
 
 const StageOne = () => {
+  const context = useContext(MyContext);
+
   return (
-    <View>
-      <Text>Stage One</Text>
-    </View>
+    <>
+      <Formik
+        initialValues={{player: ''}}
+        validationSchema={Yup.object({
+          player: Yup.string()
+          .min(3, 'Must be more than 3 characters')
+          .max(15, 'Must be less than 15 characters')
+          .required('This field is required')
+        })}
+        onSubmit={(values, { resetForm }) => {
+          context.addPlayer(values.player)
+          resetForm();
+        }}
+      >
+        {({handleChange, handleBlur, handleSubmit, values, touched, errors }) => (
+          <>
+            <Text>Who pays the bill?</Text>
+            <Input 
+              placeholder='Add names here'
+              leftIcon={{type: 'antdesign', name: 'adduser'}}
+              inputContainerStyle={{
+                marginHorizontal: 50,
+                marginTop: 50
+              }}
+              renderErrorMessage={errors.play && touched.player}
+              errorMessage={errors.player}
+              errorStyle={{
+                marginHorizontal: 50
+              }}
+              onChangeText={handleChange('player')}
+              onBlur={handleBlur('player')}
+              value={values.player}
+            />
+            <Button 
+              title='Add player'
+              onPress={handleSubmit}
+            />
+          </>
+        )}
+      </Formik>
+    </>
   );
 }
 
