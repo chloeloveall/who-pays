b0db3003bb63bcf587f72072b79ff44869070a72
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-10 23:53:46 -0700

    Import resuable main logo, use context, add buttons that use reset / new loser functions, add styles

--- src/components/StageTwo.js
+++ src/components/StageTwo.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {  Button } from 'react-native-elements';
+import { MyContext } from '../context';
+import { MainLogo } from '../utils/tools';
 
 const StageTwo = () => {
+  const context = useContext(MyContext);
+
   return (
-    <View>
-      <Text>Stage Two</Text>
-    </View>
+    <>
+      <MainLogo />
+      <Text style={styles.result}>{context.state.result}</Text>
+      <Button
+        buttonStyle={styles.button}
+        title='Try again'
+        onPress={()=> context.getNewLoser()}
+      />
+      <Button
+        buttonStyle={styles.button}
+        title="Start over"
+        onPress={()=> context.resetGame()}
+      />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#DB3EB1',
+    marginTop: 40,
+    width: '100%',
+    padding: 10
+  },
+  result: {
+    marginTop: 30,
+    fontSize: 30
+  }
+})
 
 export default StageTwo;
